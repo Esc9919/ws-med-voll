495b4c82e93f29d8b79e71f88eff402fb2a34f38
Esc9919 <Emersoncaldeira1999@hotmail.com>
2023-02-15 23:49:13 -0300

    subindo atividades do dia 15-02

---
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,32 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String lougradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    public Endereco(DadosEndereco dados) {
+        this.lougradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.cidade = dados.cidade();
+        this.complemento = dados.complemento();
+
+    }
+}
