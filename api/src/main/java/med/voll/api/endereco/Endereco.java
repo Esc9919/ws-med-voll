42619cf99944002f2d22bbf2965f026e6d3ee8ea
Esc9919 <Emersoncaldeira1999@hotmail.com>
2023-02-16 23:36:11 -0300

    subindo atividade do dia 16-02

--- api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Endereco {
 
-    private String lougradouro;
+    private String logradouro;
     private String bairro;
     private String cep;
     private String numero;
@@ -20,13 +20,13 @@ public class Endereco {
     private String uf;
 
     public Endereco(DadosEndereco dados) {
-        this.lougradouro = dados.logradouro();
+        this.logradouro = dados.logradouro();
         this.bairro = dados.bairro();
         this.cep = dados.cep();
         this.numero = dados.numero();
         this.complemento = dados.complemento();
         this.cidade = dados.cidade();
-        this.complemento = dados.complemento();
+        this.uf = dados.uf();
 
     }
 }
