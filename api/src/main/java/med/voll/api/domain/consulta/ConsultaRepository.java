8efb409c85c48f6ec864df8bb4cd6923a643e307
Esc9919 <Emersoncaldeira1999@hotmail.com>
2023-03-05 11:13:33 -0300

    Criação da regra de negócios

--- api/src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ api/src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -2,5 +2,10 @@ package med.voll.api.domain.consulta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    Boolean exixtsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHoraroio);
+
+    Boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
 }
--- api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -26,4 +26,12 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             limit 1
             """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
+
+    @Query("""
+            select m.ativo
+            from Mediico m
+            where
+            m.id = :id
+            """)
+    Boolean findAtivoById(Long idMedico);
 }
